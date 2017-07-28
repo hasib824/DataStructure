a66ea1aa47be0b037134de1ef6e840277cc9bf2d
hasib824 <hasib824@gmail.com>
2017-07-28 06:07:37 +0600

    Update Added
    Update Added

--- C++ Implementations/SegmentTree.cpp
+++ C++ Implementations/SegmentTree.cpp
@@ -1,73 +1,98 @@
-#include<stdio.h>
+#include<bits/stdc++.h>
 using namespace std;
+int A[100];
 
-const int N = 1e5;
-int tree[2*N];
-
-int  nmbrOfData; // user decided number
-
-
-void printTree()
+// Building the tree while Scanning the Tree;
+void buildTree(int n)
 {
-    for(int i=nmbrOfData-1;i>0;i--)
-        printf("Parent : %d \n left: %d ,  Right : %d\n",i,tree[i*2],tree[i*2+1]);
+   for(int i=(n/2);i<n;i++)
+   {
+       int a,b;
+       scanf("%d%d",&a,&b);
+       A[i*2] = a; // left child insertion
+       A[i*2+1] = b; // left child insertion
+       A[i]=a+b;    //  Adding the addition to root;
+   }
+   for(int i=n/2-1;i>0;i--)
+   {
+       A[i] = A[i*2] + A[i*2+1];
+   }
 }
 
-void build()
-{
-    for(int i=nmbrOfData-1;i>0;i--) tree[i]=tree[i*2]+tree[i*2+1];
-    printTree();
+
+int RangeSum(int left,int right)
+{   printf("Left %d , right %d\n",left,right);
+    int sum=0;
+    while(left<=right)
+    {
+       if(left%2==1)
+       {
+         printf("Left %d , value %d \n",left,A[left]);
+         sum+= A[left++];
+       }
+       if(right%2==0)
+       {
+           printf("right %d , value %d\n",right,A[right]);
+          sum+= A[right--];
+       }
+
+       left/=2; right/=2;
+    }
+
+    return sum;
 }
 
-void update(int number,int position)
+void update(int pos,int data)
 {
-   tree[position] = number;
-   for(int i=position;i>0;i=i/2) tree[i/2] = tree[i]+tree[i^1] ; // if I is odd then i^1 gets even and If I is Odd then i^1 gets odd
+   A[pos] = data;
+   while(pos>1)
+   {
+        pos=pos/2;
+        A[pos] = A[pos*2]+A[pos*2+1];
+   }
+
 }
 
-void RmQ(int left,int right)
-{
-  int res=0; left+=nmbrOfData; right+=nmbrOfData;
-  while(left<=right)
-  {
-      if(left&1) res += tree[left++];
-      if(!(right&1)) res+= tree[right--];
-      left/= 2; right/=2;
-  }
-  printf("\nThe result is : %d",res);
 
+void printTree(int n)
+{
+    for(int i=0;i<n*2;i++) printf("%d , ",A[i]);
 }
 int main()
 {
-    scanf("%d",&nmbrOfData);
-    int limit = 2*nmbrOfData;
-    for(int i=nmbrOfData; i<limit ;i++) scanf("%d",&tree[i]);
-    build();
-    int number, position ;
-    //printf("Please enter number and position to update : ");scanf("%d %d", &number, &position);
-   // update(number,position+nmbrOfData);
-   // printTree();
-    int left,right;
-    printf("Please enter range to calculate sum : ");scanf("%d %d", &left, &right);
-    RmQ(left,right);
+    printf("How many Numbers : \n");
+    int n; scanf("%d",&n);
+    buildTree(n);  printTree(n);
+    printf("\nPlease enter range query : from 0 to %d\n",n-1);
+    int left , right;
+    scanf("%d%d",&left,&right);
+    printf("\nSum is : %d\n",RangeSum(n+left,n+right));
+    printf("Value and position to update : \n");
+    int pos,data; scanf("%d%d",&pos,&data);
+    update(pos+n,data);
+    printTree(n);
     return 0;
 }
 
-/*
-13
-2
-3
-5
-8
-11
-7
-6
-13
-4
-9
-10
-1
-14
 
-3 9
+/*
+12
+2
+4
+5
+7
+9
+8
+13
+11
+6
+15
+3
+1
+2
+8
+
+7
+23
+
 */
