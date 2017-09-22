0ea46ebf60c5a95c0f7759030561c37276e38725
hasib824 <hasib824@gmail.com>
2017-09-22 21:51:38 +0600

    Completed
    Completed

--- C++ Implementations/IntersectionOfSortedLinkedList.cpp
+++ C++ Implementations/IntersectionOfSortedLinkedList.cpp
@@ -1,3 +1,5 @@
+// Creates a New LinkedList from the common values of two Sorted LinkedList
+
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -76,7 +78,6 @@ Node* partitioning(Node *head, Node* tail)
 
     return pNode;
 }
-int i=0;
 
 void Quicksort(Node *head, Node* tail)
 {
@@ -87,44 +88,97 @@ void Quicksort(Node *head, Node* tail)
             Quicksort(head,pivot->prev);
         if(pivot != tail)
             Quicksort(pivot->next,tail);
-
     }
 
 }
 
+// Getting the intersection of two List
+LinkedList intersection(LinkedList llOne,LinkedList llTwo)
+{
+    Node *llOneHead = llOne.head;
+    Node *llTwoHead = llTwo.head;
+    LinkedList commonList ;
+    while(llOneHead != NULL && llTwoHead != NULL)
+    {
+        if(llOneHead->data == llTwoHead->data)
+        {
+            commonList.add(llOneHead->data);
+            llOneHead = llOneHead->next;
+            llTwoHead = llTwoHead->next;
+        }
+        else if(llOneHead->data < llTwoHead->data) llOneHead = llOneHead->next;
+        else llTwoHead = llTwoHead->next;
+
+    }
+
+    return commonList;
+}
+
+
 int main()
 {
-    printf("How many datas : ");
-    int n; scanf("%d",&n);
-    LinkedList ll ;
-    for(int i=0;i<n;i++)
+    int j=0;
+    printf("How many LinkedList ? ");
+    int numberOfLinkedList; scanf("%d",&numberOfLinkedList);
+    LinkedList ll[numberOfLinkedList];
+    while(j<numberOfLinkedList)
     {
-         int a; scanf("%d",&a);
-         ll.add(a);
+        printf("\nHow many datas for list : ");
+        int n; scanf("%d",&n);
+        printf("Enter datas for list : ");
+        for(int i=0;i<n;i++)
+         {
+             int a; scanf("%d",&a);
+             ll[j].add(a);
+         }
+        printf("\n");
+        ll[j].printer();
+        Quicksort(ll[j].head,ll[j].tail);
+        printf("\nSorted data ") ;ll[j].printer();
+        j++;
+
     }
-    printf("\n");
-    ll.printer();
 
-    Quicksort(ll.head,ll.tail);
+    // Check for Intersection of the linkedLists
 
-    printf("\nSorted data ") ;ll.printer();
+        LinkedList commonList = intersection(ll[0],ll[1]);
+        printf("\n\nNew List is : ");
+        commonList.printer();
+        printf("\n");
+
     return 0;
 }
 
 
 /*
+2
 
 10
 45
 7
-87
+54
 65
 98
 23
-14
-4
+63
+74
 62
+100
+
+13
+4
+71
+45
+23
+89
+12
+98
+65
+48
 15
+74
+91
+46
 
 
 
