a6e911a176facba6ff2cd94c379c31823184a3ba
hasib824 <hasib824@gmail.com>
2017-09-22 16:17:28 +0600

    Done
    Done

--- C++ Implementations/IntersectionOfSortedLinkedList.cpp
+++ C++ Implementations/IntersectionOfSortedLinkedList.cpp
@@ -2,12 +2,7 @@
 
 using namespace std;
 
-
-
-class LinkedList
-{
-
-    struct Node
+struct Node
    {
        Node *prev;
        int data;
@@ -20,10 +15,12 @@ class LinkedList
 
    };
 
+// Implementing Doubly Linked List
+class LinkedList
+{
+   public:
    Node *head = NULL ;
    Node *tail = NULL ;
-
-   public:
     void add(int a)
     {
         Node *temp = new Node(a,head);
@@ -41,16 +38,59 @@ class LinkedList
           temp = temp->next;
         }
         temp = tail;
-        printf("\nPrinting Reversly : ");
+       /* printf("\nPrinting Reversly : ");
         while(temp!=NULL)
         {
             printf("%d, ",temp->data);
             temp = temp->prev;
         }
+        */
     }
 
 };
 
+
+// Sorting
+void swapping(Node* first,Node* second)
+{
+        int data = first->data;
+        first->data = second->data;
+        second->data= data;
+}
+
+Node* partitioning(Node *head, Node* tail)
+{
+    Node* pNode = head;
+    Node* traverser = head;
+    int data = tail->data;
+    while( traverser!= tail)
+    {
+        if(traverser->data<data)
+        {
+           swapping(pNode,traverser);
+           pNode = pNode->next;
+        }
+        traverser = traverser->next;
+    }
+    swapping(pNode,tail);
+
+    return pNode;
+}
+int i=0;
+
+void Quicksort(Node *head, Node* tail)
+{
+    if(head!=tail)
+    {
+        Node *pivot = partitioning(head,tail);
+        if(pivot != head)
+            Quicksort(head,pivot->prev);
+        if(pivot != tail)
+            Quicksort(pivot->next,tail);
+
+    }
+
+}
 
 int main()
 {
@@ -62,9 +102,12 @@ int main()
          int a; scanf("%d",&a);
          ll.add(a);
     }
-
+    printf("\n");
     ll.printer();
 
+    Quicksort(ll.head,ll.tail);
+
+    printf("\nSorted data ") ;ll.printer();
     return 0;
 }
 
@@ -72,16 +115,17 @@ int main()
 /*
 
 10
-8
+45
 7
-5
-6
-4
+87
+65
+98
 23
-7
-8
-9
-46
+14
+4
+62
+15
+
 
 
 
