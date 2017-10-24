5b0a6685804a7bd1e53750c65bbbefee52cbbae0
hasib824 <hasib824@gmail.com>
2017-10-24 11:30:11 +0600

    dd
    dd

--- Java_Implementations/Graph/Dijakstra/Dijakstra/src/dijakstra/MyPriorityQueue.java
+++ Java_Implementations/Graph/Dijakstra/Dijakstra/src/dijakstra/MyPriorityQueue.java
@@ -5,24 +5,25 @@ public class MyPriorityQueue<T>
 {   
 	Comparator comparator;
         ArrayList<T> datas;
-        int size = -1;
-	MyPriorityQueue(int numberOfNodes,Comparator mycomparator)
+        int size = 0;
+	MyPriorityQueue(Comparator mycomparator)
 	{
 		this.comparator = mycomparator;
-		this.datas = new ArrayList();
+		datas = new ArrayList<T>();
 	}
 	
 	void push(T data)
-	{
-		datas.add(++size, data);
+	{       
+		datas.add(data);
+                size = datas.size()-1;
 		int l = size;
 		while(l>=0)
 		{	T child = datas.get(l);
 			T parent = datas.get(l);
 			if(0>comparator.compare(child,parent))
 			{
-                            datas.add(l/2, child);
-                            datas.add(l,parent);
+                            datas.set(l/2, child);
+                            datas.set(l,parent);
 			}
 		}
 	}
@@ -33,7 +34,7 @@ public class MyPriorityQueue<T>
 	
 	void pop()
 	{
-            datas.add(0, datas.get(size));
+            datas.set(0, datas.get(size));
             heapify(0,size);
 	}
 	
@@ -48,21 +49,9 @@ public class MyPriorityQueue<T>
 		l = pos*2+2;
             if(l!=pos)
             {
-                datas.add(pos,datas.get(l));
-                datas.add(l,parent);
+                datas.set(pos,datas.get(l));
+                datas.set(l,parent);
                 heapify(l,size);
             }
 	}	
 }
-
-
-
-
-
-
-
-
-
-
-
-
